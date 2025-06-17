d888a6402ae8f8a5d89747cff78b2aa2949046be
nikamshr <shreyas.nikam@ikione.com>
2025-06-17 14:10:14 +0530

    had to go back to js because of ts config issues

--- app/schema/user.schema.js
+++ app/schema/user.schema.js
@@ -1,16 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userModel = void 0;
-var mongoose_1 = require("mongoose");
-var userSchema = new mongoose_1.default.Schema({
-    username: {
-        required: true,
-        type: String,
-        unique: true,
-    },
-    password: {
-        required: true,
-        type: String,
+import mongoose from 'mongoose' ;
+
+const userSchema = new mongoose.Schema({
+    username : {
+        required : true,  
+        type : String  , 
+        unique : true , 
+    }, 
+    password : { 
+        required : true , 
+        type : String , 
     }
-});
-exports.userModel = mongoose_1.default.model("user", userSchema);
+})
+
+export const userModel = mongoose.model("user", userSchema) ; 
--- app/zod_schema/signUp.zod.js
+++ app/zod_schema/signUp.zod.js
@@ -1,8 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var zod = require("zod");
-var signUpZodSchema = zod.object({
-    username: zod.string().min(3, "Username should not be less than 3 characters !").max(30, "Username should be less than 30 characters"),
-    password: zod.string().min(8, "Password should be longer than 8 characters! ")
-});
-exports.default = signUpZodSchema;
+import * as zod from 'zod' ;
+
+
+const signUpZodSchema = zod.object({
+    username : zod.string().min(3, "Username should not be less than 3 characters !").max(30, "Username should be less than 30 characters") , 
+    password : zod.string().min(8, "Password should be longer than 8 characters! ") 
+})
+
+export default signUpZodSchema
